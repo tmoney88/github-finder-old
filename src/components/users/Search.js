151e24bd35ba37cbc42acc48550b584e14e6f227
Tanner Gates <tgates4@dmail.dixie.edu>
2020-06-09 22:45:36 -0600

    Convert Search.js and User.js to Functional components

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
